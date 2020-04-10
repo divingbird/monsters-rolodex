8f525bffc179eba1a09e5b6b43da0b54af602818
divingbird <54239734+divingbird@users.noreply.github.com>
2020-04-10 21:42:56 +0100

    Updates

---
+++ precache-manifest.3eca5dd39444d1192946609a3984538e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48fd5e83acbb098b0623d7a9ff43d3e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6b4c7eb64759d89f931",
+    "url": "/monsters-rolodex/static/css/main.c42b5fa7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b4c7eb64759d89f931",
+    "url": "/monsters-rolodex/static/js/main.46ae3864.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
